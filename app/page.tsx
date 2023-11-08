791d768e1630ff1f9b72b3cfba68d181879bbef9
Santiago Crippa <santicrippa@gmail.com>
2023-11-08 20:42:08 -0300

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
 import Link from 'next/link';
+import { CardSkeleton } from './ui/skeletons';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -24,6 +25,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+        <CardSkeleton />
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
