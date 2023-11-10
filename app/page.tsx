7d12a14f9e4a020c31c745adda50501a56a74c13
Santiago Crippa <santicrippa@gmail.com>
2023-11-10 00:49:12 -0300

    loading fonts and hero images

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
-import Link from 'next/link';
-import { CardSkeleton } from './ui/skeletons';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -25,9 +26,21 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <CardSkeleton />
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src='/hero-desktop.png'
+            alt='Screenshots of the dashboard'
+            width={1000}
+            height={760}
+            className='hidden md:block'
+          />
+          <Image
+            src='/hero-mobile.png'
+            alt='Screenshots of the dashboard'
+            width={560}
+            height={620}
+            className='block md:hidden'
+          />
         </div>
       </div>
     </main>
