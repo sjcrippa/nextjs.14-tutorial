1869524327f6fd17fc4383ee3381b9fc6b5e1f47
Santiago Crippa <santicrippa@gmail.com>
2023-11-09 23:37:24 -0300

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to La concha de tu vieja.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              Next.js Learn Course Bien puta
             </a>
             , brought to you by Vercel.
           </p>
