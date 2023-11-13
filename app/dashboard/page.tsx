4cdad9dc16309a2f7784d08e7df612d7c85114cc
Santiago Crippa <santicrippa@gmail.com>
2023-11-13 12:22:51 -0300

    adding nested routes and links

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardPage() {
+  return <p>Contenido del dashboard</p>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
         {children}
+
+        <footer className='py-10 flex justify-center items-center'>
+          Made with ❤️ by S.J. Crippa 
+        </footer>
+        
       </body>
     </html>
   );
